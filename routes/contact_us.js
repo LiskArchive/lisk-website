e9eeca173352c6a268da942bd32a870c4a7215f0
Olivier Beddows <olivier@lisk.io>
2016-01-21 02:00:59 +0200

    Renaming route from contact-us to contact_us.

--- routes/contact_us.js
+++ routes/contact_us.js
@@ -9,11 +9,11 @@ var transporter = nodemailer.createTransport({
 });
 
 module.exports = function (app) {
-	app.get("/contact-us", function (req, res) {
+	app.get("/contact_us", function (req, res) {
 		return res.render("contact", {isMap: true, title: "Contact Us",  description: "If you need to contact the Lisk team for business inquiries or personal matters, here you can.", words: "lisk, crypti, contact, dapp, dapps, decentralized application, dapp store, crypto, currency, cryptocurrency, smart contracts, smart contract, decentralized applications, wallet, blockchain"});
 	});
 
-	app.post('/contact-us', function (req, res) {
+	app.post('/contact_us', function (req, res) {
 		var name = req.body.name,
 			email = req.body.email,
 			company = req.body.company,
