280ea008b5458428bafc4f45f5e2550b6414ac3e
Olivier Beddows <olivier@lisk.io>
2016-01-24 12:36:11 +0000

    Removing grunt-contrib-jade.

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,22 +53,6 @@ module.exports = function(grunt){
 				}
 			}
 		},
-		jade   : {
-			compile: {
-				options: {
-					client: false,
-					pretty: true
-				},
-				files  : [{
-					cwd   : "jade/",
-					src   : "*.jade",
-					dest  : "",
-					expand: true,
-					ext   : ".html"
-				}]
-			}
-		},
-
 		watch  : {
 			scripts: {
 				files  : ['public/js/*.js'],
@@ -94,11 +78,9 @@ module.exports = function(grunt){
 		}
 	});
 
-
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-jade');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
