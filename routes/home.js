6a54ca088942bccb3c2deee78e02e8684fea480d
Olivier Beddows <olivier@lisk.io>
2016-01-24 21:04:10 +0000

    Removing commented code.

--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,3 @@
-/*var partners = [
-	{
-		name: "Sia - Decentralized Storage",
-		site: "http://www.siacoin.com/",
-		image: "i/sia.png"
-	}
-]*/
-
 module.exports = function (app) {
 	app.get("/", function (req, res) {
 		res.render('index', {pageId: "home", title: "Decentralized Application Platform", description: "Develop and publish decentralized applications with your own side chains on the open-source Lisk Platform.", words: "lisk, crypti, dapp, dapps, decentralized application, dapp store, crypto, currency, cryptocurrency, smart contracts, smart contract, decentralized applications, wallet, blockchain, sia", hasScript: true});
