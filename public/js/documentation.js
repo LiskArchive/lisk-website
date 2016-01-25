7e7e7ef1a929d52d6188d3df6160700975c681c6
Olivier Beddows <olivier@lisk.io>
2016-01-25 22:21:24 +0000

    Removing conflicting code.
    Fixes prism.js code block rendering problem.

--- public/js/documentation.js
+++ public/js/documentation.js
@@ -21,21 +21,4 @@ $(function($){
 		body_var.toggleClass('open_aside');
 		return false;
 	});
-
-	$("code").parent().append("<pre></pre>");
-
-	$("code").each(function () {
-		$(this).appendTo($(this).parent().find("pre"));
-		var text = $(this).text();
-		var strs = text.split('\n');
-		var lang = syntax[strs[0]];
-		if (!lang) {
-			lang = "markup";
-		}
-
-		$(this).addClass("language-" + lang);
-		strs.splice(0, 1);
-		text = strs.join('\n');
-		$(this).text(text);
-	});
 });
