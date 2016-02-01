1acbcbc3bd3a85a7ab2953ee446abeed5a340332
Max Kordek <me@maxkordek.com>
2016-02-01 19:32:23 +0100

    Update mediakit.js

--- routes/mediakit.js
+++ routes/mediakit.js
@@ -1,5 +1,5 @@
 module.exports = function (app) {
 	app.get('/mediakit', function (req, res) {
-		return res.render('mediakit', {pageId: "mediakit", title: "Media Kit", hasScript: false});
+		return res.render('mediakit', {pageId: "mediakit", title: "Media Kit", description: "The official Lisk Media Kit with many logos and related images.", words: "lisk, media kit, mediakit, crypti, dapp, dapps, decentralized application, dapp store, crypto, currency, cryptocurrency, smart contracts, smart contract, decentralized applications, wallet, blockchain", hasScript: false});
 	});
 }
