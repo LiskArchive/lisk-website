d92fbd5ba41e53e5e45fee984b80472fe5ac33b7
Max Kordek <me@maxkordek.com>
2016-02-09 17:15:05 +0100

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,14 @@
+var partnerships = [
+	{
+		name: "..",
+		img: "..",
+		url: "https://"
+	},
+];
+
+
 module.exports = function (app) {
 	app.get("/", function (req, res) {
-		res.render('home', {pageId: "home", title: "Decentralized Application Platform", description: "Develop and publish decentralized applications with your own side chains on the open-source Lisk Platform.", words: "lisk, crypti, dapp, dapps, decentralized application, dapp store, crypto, currency, cryptocurrency, smart contracts, smart contract, decentralized applications, wallet, blockchain, sia", hasScript: true});
+		res.render('home', {pageId: "home", title: "Decentralized Application Platform", partnerships: partnerships, description: "Develop and publish decentralized applications with your own side chains on the open-source Lisk Platform.", words: "lisk, crypti, dapp, dapps, decentralized application, dapp store, crypto, currency, cryptocurrency, smart contracts, smart contract, decentralized applications, wallet, blockchain, sia", hasScript: true});
 	});
 }
