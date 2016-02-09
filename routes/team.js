ec1113e3d182aa064c7b799d265df1b3d41133e6
Max Kordek <me@maxkordek.com>
2016-02-09 16:00:41 +0100

    Update team.js

--- routes/team.js
+++ routes/team.js
@@ -1,54 +1,11 @@
-var members = [
+var adviser = [
 	{
-		name: "Max Kordek",
-		post: "CHIEF EXECUTIVE OFFICER",
-		linked: "https://www.linkedin.com/profile/view?id=215859616&locale=en_US",
-		twitter: "http://twitter.com/MaxKordek"
-	},
-	{
-		name: "Max Kordek",
-		post: "CHIEF EXECUTIVE OFFICER",
-		twitter: "http://twitter.com/MaxKordek",
-		btt: "https://bitcointalk.org/index.php?action=profile;u=553440"
-	},
-	{
-		name: "Olivier Beddows",
-		post: "CHIEF TECHNICAL OFFICER",
-		btt: "https://bitcointalk.org/index.php?action=profile;u=205168",
-		twitter: "http://twitter.com/karmacrypto"
-	},
-	{
-		name: "Olivier Beddows",
-		post: "CHIEF TECHNICAL OFFICER",
-		btt: "https://bitcointalk.org/index.php?action=profile;u=205168",
-		twitter: "http://twitter.com/karmacrypto"
-	}
-];
-
-var partnerships = [
-	{
-		name: "abc",
-		date: "Since February 2016",
-		web: "http://abc.com/",
-		card: "abc"
-	},
-	{
-		name: "abc",
-		date: "Since March 2016",
-		web: "http://abc.org/",
-		card: "abc"
-	},
-	{
-		name: "abc",
-		date: "Since February 2016",
-		web: "http://abc.com/",
-		card: "abc"
-	},
-	{
-		name: "abc",
-		date: "Since March 2016",
-		web: "http://abc.org/",
-		card: "abc"
+		name: "Boris Povod",
+		img : "boris_povod",
+		company: "Crypti",
+		description: "Lead Developer",
+		linked: "https://www.linkedin.com/in/boris-povod-361a1b79/en",
+		twitter: "https://twitter.com/CryptiTeam"
 	}
 ];
 
@@ -68,15 +25,19 @@ var correspondents = [
 		country: "China",
 		btt: "https://bitcointalk.org/index.php?action=profile;u=306645",
 		pm: "https://bitcointalk.org/index.php?action=pm;sa=send;u=306645"
+	},
+	{
+		name: "Rendra Permana",
+		lang: "indonesia",
+		country: "Indonesia",
+		twitter: "https://twitter.com/rendravolt",
+		btt: "https://bitcointalk.org/index.php?action=profile;u=384646",
+		pm: "https://bitcointalk.org/index.php?action=pm;sa=send;u=384646"
 	}
 ];
 
 module.exports = function (app) {
 	app.get("/team", function (req, res) {
-		members.forEach(function (item) {
-			item.photo = item.name.toLowerCase().replace(' ', '');
-		});
-
-		return res.render("team", {pageId: "team", title: "Team", correspondents: correspondents, members : members, partnerships: partnerships, description: "Take a look at the team which makes hard problems simple and maintain Lisk on a regular basis.", words: "lisk, crypti, contact, team, dapp, dapps, decentralized application, dapp store, crypto, currency, cryptocurrency, smart contracts, smart contract, decentralized applications, wallet, blockchain", hasScript: false});
+		return res.render("team", {pageId: "team", title: "Team", correspondents: correspondents, adviser : adviser, description: "Take a look at the team which makes hard problems simple and maintain Lisk on a regular basis.", words: "lisk, crypti, contact, team, dapp, dapps, decentralized application, dapp store, crypto, currency, cryptocurrency, smart contracts, smart contract, decentralized applications, wallet, blockchain", hasScript: false});
 	});
 }
