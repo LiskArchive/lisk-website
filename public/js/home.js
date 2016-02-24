9da6c7dc288c1c915bec2e23be431093d76c9d0f
Olivier Beddows <olivier@lisk.io>
2016-02-24 20:49:23 +0000

    Beautifying code.

--- public/js/home.js
+++ public/js/home.js
@@ -1,8 +1,8 @@
 var stepSection,
     stepSlider;
 
-$(function($){
-	$("#investNowBtn").on('click', function () {
+$(function($) {
+	$("#investNowBtn").on('click', function() {
 		$('html, body').animate({
 			scrollTop: $("#investNow").offset().top - 70
 		}, 2000);
@@ -28,17 +28,17 @@ $(function($){
 		infinite     : false
 	});
 
-	stepSlider.on('beforeChange', function(event, slick, currentSlide, nextSlide){
+	stepSlider.on('beforeChange', function(event, slick, currentSlide, nextSlide) {
 		stepSection.attr('data-step', nextSlide + 1);
 	});
 
-  $('.stepBtn').on('click', function(){
+  $('.stepBtn').on('click', function() {
 		var firedEl = $(this), ind = firedEl.index();
 		stepSlider.slick('slickGoTo', parseInt(ind));
 		return false;
 	});
 
-	$('.discoverBtn').on ('click', function () {
+	$('.discoverBtn').on ('click', function() {
 		scrollDoc(browserWindow.height() - nav_container.height(), 800);
 
 		return false;
