6be7ceef9465de4daf2be76a600b9cfb4634a605
Max Kordek <me@maxkordek.com>
2016-05-13 12:37:04 +0200

    Delete faq.js

--- routes/faq.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (app) {
-	app.get("/faq", function (req, res) {
-		return res.render("faq", {pageId: "faq", title: "Frequently Asked Questions",  description: "Frequently asked questions regarding Lisk, the decentralized application platform.", words: "lisk, crypti, documentation, faq, frequently asked questions, dapp, dapps, decentralized application, dapp store, crypto, currency, cryptocurrency, smart contracts, smart contract, decentralized applications, wallet, blockchain", hasScript: false});
-	})
-}
