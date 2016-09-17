ea0e55b56e7a722e11504ca737073ae2bce4b3ea
Max Kordek <max@lisk.io>
2016-09-17 17:05:07 +0200

    Updated to 0.3.4

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ var hbs = exphbs.create({
 			return "https://lisk.io/documentation?i=lisk-docs/BinaryInstall";
 		},
 		version : function () {
-			return "v0.3.3";
+			return "v0.3.4";
 		},
 		blog : function () {
 			return "http://blog.lisk.io";
