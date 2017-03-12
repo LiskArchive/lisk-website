4d92d050d0deb28e28f4aa8231e2674a2856ebe3
Oliver Beddows <oliver@lisk.io>
2017-03-12 11:26:36 +0100

    Removing ico page

--- routes/ico.js
+++
@@ -1,5 +0,0 @@
-module.exports = function (app) {
-	app.get("/ico", function (req, res) {
-		return res.render("ico", {pageId: "ico", title: "Initial Coin Offering", description: "Beautiful and simple on the front, secure and brilliant on the back. Take a look at Lisk’s awesome features.", words: "lisk, crypti, features, source code, open source, development, dapp, dapps, decentralized application, dapp store, crypto, currency, cryptocurrency, smart contracts, smart contract, decentralized applications, wallet, blockchain", hasScript: true});
-	})
-}
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,6 @@ module.exports = {
 	features : require('./features.js'),
 	foundation: require('./foundation.js'),
 	contact_us: require('./contact_us.js'),
-	ico: require('./ico.js'),
 	careers: require('./careers.js'),
 	delegates: require('./delegates.js'),
 	missing: require('./missing.js')
