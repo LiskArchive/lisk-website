a578e1f949c09f0a8074c82b4a57ef99211d6c43
Oliver Beddows <oliver@lightcurve.io>
2017-04-12 16:16:31 +0200

    Bumping version

--- app.js
+++ app.js
@@ -118,7 +118,7 @@ var hbs = exphbs.create({
 			return "https://github.com/LiskHQ/lisk-wiki/wiki/Binary-Install";
 		},
 		version : function () {
-			return "v0.7.0";
+			return "v0.8.0";
 		},
 		blog : function () {
 			return "http://blog.lisk.io";
