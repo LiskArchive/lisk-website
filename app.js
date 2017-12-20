218e9efc965f4ba85739970ad3f674b2c349522a
Vit Stanislav <vit@lightcurve.io>
2017-12-20 12:03:11 +0100

    Bump Lisk Nano version to 1.3.1

--- app.js
+++ app.js
@@ -107,13 +107,13 @@ var hbs = exphbs.create({
 			return "50";
 		},
 		nano_macos : function () {
-			return "https://github.com/LiskHQ/lisk-nano/releases/download/v1.3.0/lisk-nano-mac-1.3.0.dmg";
+			return "https://github.com/LiskHQ/lisk-nano/releases/download/v1.3.1/lisk-nano-mac-1.3.1.dmg";
 		},
 		nano_windows : function () {
-			return "https://github.com/LiskHQ/lisk-nano/releases/download/v1.3.0/lisk-nano-win-1.3.0.exe";
+			return "https://github.com/LiskHQ/lisk-nano/releases/download/v1.3.1/lisk-nano-win-1.3.1.exe";
 		},
 		nano_linux : function () {
-			return "https://github.com/LiskHQ/lisk-nano/releases/download/v1.3.0/lisk-nano-1.3.0-x86_64.AppImage";
+			return "https://github.com/LiskHQ/lisk-nano/releases/download/v1.3.1/lisk-nano-1.3.1-x86_64.AppImage";
 		},
 		docker : function () {
 			return "https://github.com/LiskHQ/lisk-wiki/wiki/Docker-Install";
